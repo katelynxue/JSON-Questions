06bb7d60bf9d9451638d60775c0ff117923d8eee
katelynxue <katelyn_xue26@milton.edu>
2025-11-06 08:38:50 -0500

    worked on in class

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ console.log(data);
 console.log(data[57]);
 console.log(data[57].title);
 
-const filteredZ = data.filter(book => book.title !== "zzzzzz");
-console.log(filteredZ);
+const filteredBooks = data.filter(book => 
+    book.title !== "zzzzzz" &&
+    book.title !== ""
 
-const filteredEmpty = data.filter(book => book.title !=="");
-console.log(filteredEmpty);
+);
+console.log(filteredBooks);
+
 // 1. avg time it takes me to read a book
 const filteredReadBooks = data.filter(book => 
   book["Time on TBR (days)"] !== "N/A" && 
@@ -15,19 +17,34 @@ const filteredReadBooks = data.filter(book =>
 );
 
 let sum = 0;
-for (let i = 0; i++; i<=data.length){
-    sum += arr[i];
+for (let i = 0; i<filteredReadBooks.length; i++){
+    sum += Number(filteredReadBooks[i]["Time on TBR (days)"]);
 }
-return sum / arr.length;
+const averageTime = sum / filteredReadBooks.length;
 
+console.log(averageTime);
 
 // 2. % of books that are read
+const readBooks = filteredBooks.filter(book => book["read?"] !== "No" );
+console.log(readBooks);
+
+const NumberOfReadBooks = readBooks.length;
+console.log(NumberOfReadBooks);
+
+const totalBooks = filteredBooks.length;
+const fractionOfBooksRead = NumberOfReadBooks / totalBooks;
+console.log(fractionOfBooksRead);
+const percentOfBooksRead = fractionOfBooksRead * 100;
+console.log(percentOfBooksRead);
+
 // 3. what my fav genre based on how many purchaes
-// 4. what year have i quired the most books
+const allGenres = filteredBooks.map(book => book.category);
+console.log(allGenres);
+const genres = [...new Set(allGenres)];
+console.log(genres);
+
+
+
+// 4. what year have i aquired the most books
 // 5. come up with your own stat
 
-
-
-// fetch('./rawBooks.json')
-//     .then((response) => response.json())
-//     .then((json) => console.log(json));
